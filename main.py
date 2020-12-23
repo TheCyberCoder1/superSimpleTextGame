6412bb11c8b5e95f50b40dcefbecc7584f7abfe8
Riyaan Sharma <riyaan.au@gmail.com>
2020-12-23 21:07:36 +1100

    added main.py and wrote back story

---
+++ main.py
@@ -0,0 +1,4 @@
+import colorama
+user = input()
+
+print("Welcome to the \033[32mSEARCH FOR THE EVIL CEO\033[39m\nOnce upon a time an evil CEO took the money of his company, he has taken the money and hidden in his large mansion.\nWell what are you waiting for? Get seaarching!")
