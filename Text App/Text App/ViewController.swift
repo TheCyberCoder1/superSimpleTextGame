3c67b6a4335ac19970f68a764ac41f1953ff2dde
Arhan Busam <arhan.busam@gmail.com>
2020-12-24 13:01:51 +1100

    Xcode setup

---
+++ Text App/Text App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text App
+//
+//  Created by Arhan Busam on 24/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
